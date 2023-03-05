bac244dfac890f72ec7f672dbae41c2e3a68f01c
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-05 16:37:05 +0530

    Created Post Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import {Main} from "./Pages/main";
+import {Main} from "./Pages/main/main";
 import {Login} from "./Pages/login";
 import {Navbar} from "./components/navbar";
 import { CreatePost } from './Pages/create-post/create_post';
---
+++ src/Pages/main/main.tsx
@@ -0,0 +1,34 @@
+import { getDocs, collection, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../../config/firebase';
+import { Post } from './post';
+
+interface Post{
+    id: string;
+    userId: string;
+    title: string;
+    username: string;
+    description: string;
+}
+
+export const Main =() =>{
+    const [postList, setPostList] = useState<Post[] | null>(null);
+    const postRef = collection(db,"posts");
+
+    const getPost = async () => {
+        const data = await getDocs(postRef);
+        setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
+    }
+    
+    useEffect(() =>{
+        getPost();
+    },[]);
+
+    return(
+        <div>
+            {postList?.map((post) =>(
+                <Post />
+            ))}
+        </div>
+    )
+}
